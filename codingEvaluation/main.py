efb53dafd8673a01a2625eb257597d0ed34fd4f0
jiah <whatime08@pukyong.ac.kr>
2024-10-02 21:19:27 +0900

    feat: 문서 업로드 함수 구현(#2)

--- codingEvaluation/main.py
+++ codingEvaluation/main.py
@@ -1,12 +1,33 @@
-def docs():
+import os
+
+from typing import List
+
+from langchain_community.document_loaders import TextLoader
+
+
+def docs_load() -> List[str]:
     """
-    This is a docstring
+    문서를 읽는 함수
     """
-    pass
+
+    try:
+        loader = TextLoader("input/pep8.txt", encoding="utf-8").load()
+
+        docs = []
+        for doc in loader:
+            docs = doc.page_content
+
+        return docs
+    except FileNotFoundError:
+        print("파일을 찾을 수 없습니다. 경로를 확인하세요.")
+        return []
+    except Exception as e:
+        print(f"오류가 발생했습니다: {e}")
+        return []
 
 def run():
-    docs()
-
+    docs_load()
 
 if __name__ == '__main__':
     run()
+
