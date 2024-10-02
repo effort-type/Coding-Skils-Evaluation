7e3d6761d89e76a6847d22677b8644fa6e0e238f
Minsu <jminsu084@gmail.com>
2024-10-03 00:22:21 +0900

    feat: 질의응답 기능 구현 및 더미데이터 실험 완료(#10) (#11)

--- codingEvaluation/main.py
+++ codingEvaluation/main.py
@@ -9,8 +9,13 @@ from langchain_text_splitters import MarkdownHeaderTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
+from langchain_community.document_transformers import LongContextReorder
 from langchain_openai import ChatOpenAI
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 
 import tiktoken
 
@@ -131,12 +136,104 @@ def chat_llm():
     return llm
 
 
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+def reorder_documents(docs):
+    # 재정렬
+    reordering = LongContextReorder()
+    reordered_docs = reordering.transform_documents(docs)
+    combined = format_docs(reordered_docs)
+
+    return combined
+
+
+def evaluate(llm, pep8_db, pep8_bm_db, query):
+    """
+    문서를 평가하는 함수
+    """
+
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=[pep8_bm_db, pep8_db],
+        weights=[0.5, 0.5],
+        search_type="mmr",
+    )
+
+    # 질문에 대한 답변을 찾기 위한 프롬프트
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+                You are a large language model performing a crucial role in a RAG-based coding proficiency evaluation system. 
+                Your primary task is to evaluate the code input by the user. Analyze the code based on the official PEP8 coding style guide and provide feedback on style and rules. 
+                Include clear and specific explanations to help the user understand, and suggest improvements when necessary.
+
+                **Role:**
+                1. Evaluate Python code provided by the user according to the PEP8 coding style guide.
+                2. Analyze the code and provide feedback on aspects such as style, readability, and consistency.
+                3. If errors or inefficiencies are found, identify them and offer specific suggestions for correction.
+                4. Clearly explain parts of the code that violate the PEP8 coding style guide and mention the specific rules involved.
+                5. Deliver feedback as clearly and concisely as possible to ensure the user can easily understand.
+                6. When responding to questions, provide relevant examples or links to appropriate documentation related to the code.
+
+                **Instructions:**
+                - Always evaluate the code following the rules specified in the PEP8 coding style guide.
+                - Feedback should be specific and practical. For example, clearly explain issues such as variable naming conventions, indentation, line length, and so on.
+                - In cases of PEP8 guideline violations, explicitly describe which rules have been breached and offer suggestions on how to correct the code according to those rules.
+                - Provide specific and actionable suggestions to help the user write better code.
+                - Responses should be objective, with clear references to the rules that form the basis of the evaluation.
+
+                Based on these guidelines, evaluate the given code according to the PEP8 standards and clearly explain which parts violate the PEP8 coding style guide, providing feedback accordingly.
+
+                **Important**: All responses, except for the code, must be written in Korean.
+
+                **Context:** {context}
+
+                """,
+            ),
+            (
+                "human",
+                """
+                **Input Code**                
+                ```python
+                {question}
+                ```               
+                """
+            ),
+        ]
+    )
+
+    chain = {
+                "context": ensemble_retriever | RunnableLambda(reorder_documents),
+                "question": RunnablePassthrough()
+            } | prompt | llm | StrOutputParser()
+
+    response = chain.invoke(query)
+
+    if not isinstance(llm, ChatOpenAI):
+        print("\n\n{}".format(response))
+
+    return response
+
+
 def run():
     docs = docs_load()
     chunk = text_split(docs)
     pep8_db, pep8_bm_db = pep8_docs_embedding(chunk)
 
     llm = chat_llm()
+
+    query = """
+    def MyFunction():
+        a=10
+        b = 20
+        sum=a+b
+        print('Sum:', sum )
+    """
+
+    response = evaluate(llm, pep8_db, pep8_bm_db, query)
 
 
 if __name__ == '__main__':
