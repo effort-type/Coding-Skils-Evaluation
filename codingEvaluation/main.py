77b8508ad84663e3dd626c7fefd6f3db5d91a9b7
jiah <whatime08@pukyong.ac.kr>
2024-10-02 21:45:26 +0900

    feat: 마크다운 텍스트 스플리터를 사용하여 문서를 분할하는 함수 구현(#5)

--- codingEvaluation/main.py
+++ codingEvaluation/main.py
@@ -3,6 +3,7 @@ import os
 from typing import List
 
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import MarkdownHeaderTextSplitter
 
 
 def docs_load() -> List[str]:
@@ -25,8 +26,42 @@ def docs_load() -> List[str]:
         print(f"오류가 발생했습니다: {e}")
         return []
 
+
+def text_split(corpus):
+    """
+    문서를 분리하는 함수
+    """
+
+    headers_to_split_on = [  # 문서를 분할할 헤더 레벨과 해당 레벨의 이름을 정의합니다.
+        (
+            "#",
+            "Header 1",
+        ),  # 헤더 레벨 1은 '#'로 표시되며, 'Header 1'이라는 이름을 가집니다.
+        (
+            "##",
+            "Header 2",
+        ),  # 헤더 레벨 2는 '##'로 표시되며, 'Header 2'라는 이름을 가집니다.
+        (
+            "###",
+            "Header 3",
+        ),  # 헤더 레벨 3은 '###'로 표시되며, 'Header 3'이라는 이름을 가집니다.
+    ]
+
+    splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+
+    chunks = splitter.split_text(corpus)
+
+    # 토크나이저 => 2028 토큰이 가장 큰 사이즈임
+    # tokenizer = tiktoken.get_encoding("o200k_base")
+    # for chunk in chunks:
+    #     print(len(tokenizer.encode(chunk.page_content)))
+
+    return chunks
+
+
 def run():
     docs_load()
+    text_split()
 
 if __name__ == '__main__':
     run()
